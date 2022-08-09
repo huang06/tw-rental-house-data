9497b8a991fb197627dcf9411e3229da0f11693a
huang06 <tom4996131207@gmail.com>
2022-08-09 16:42:14 +0800

    feat: update

--- backend/rental/libs/export/field.py
+++ backend/rental/libs/export/field.py
@@ -1,18 +1,11 @@
-from django.contrib.postgres.fields.jsonb import KeyTextTransform
 from datetime import datetime
+
+from django.db.models.fields.json import KeyTextTransform
 from django.utils import timezone
 
-class Field():
-    def __init__(
-        self,
-        column,
-        zh,
-        en=None,
-        field=None,
-        enum=None,
-        annotate=None,
-        fn=None,
-        child_fields=[]):
+
+class Field:
+    def __init__(self, column, zh, en=None, field=None, enum=None, annotate=None, fn=None, child_fields=None):
 
         self.column = column
         self.zh = zh
@@ -23,10 +16,14 @@ class Field():
         self.child_fields = []
 
         if field:
-            self.en = '{}_{}'.format(self.en, field)
+            self.en = f'{self.en}_{field}'
 
         if not annotate and self.field:
             self.annotate = KeyTextTransform(self.field, self.column)
+
+        # avoid mutable argument issue
+        if child_fields is None:
+            child_fields = []
 
         for child in child_fields:
             self.child_fields.append(Field(**child))
@@ -40,9 +37,9 @@ class Field():
             val = timezone.localtime(val).strftime('%Y-%m-%d %H:%M:%S %Z')
         elif val == '' or val is None:
             val = '-'
-        elif val == True or val == 'true':
+        elif val is True or val == 'true':
             val = 'T' if use_tf else 1
-        elif val == False or val == 'false':
+        elif val is False or val == 'false':
             val = 'F' if use_tf else 0
 
         return val
@@ -55,9 +52,9 @@ class Field():
             pass
         elif val == '' or val is None:
             val = None
-        elif val == True or val == 'true':
+        elif val is True or val == 'true':
             val = True
-        elif val == False or val == 'false':
+        elif val is False or val == 'false':
             val = False
 
         return val
